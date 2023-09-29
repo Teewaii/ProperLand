eb4b5542ac8352f7af5464625c8eb7509c1bc817
Taiye <taiyeajimati@gmail.com>
2023-09-29 23:35:24 +0100

    Footer adjust

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
             <img className='w-[35%] absolute top-[50%] left-0 ' src={Left_blog_pattern} alt="Background pattern" />
             <img className='w-[15%] absolute bottom-top-[50%] right-0 ' src={Rigft_blog_pattern} alt="Background pattern" />
 
-            <div className="wrapper container mt-12 lg:mt-0 ">
+            <div className="wrapper container relative mt-12 lg:mt-0 ">
                 <div className="top flex flex-col gap-8 lg:border-b-2 lg:border-opacity-20 border-primary lg:justify-between lg:flex-row lg:items-center ">
                     <div className="topLeft flex flex-col items-center lg:items-start gap-8 lg:pr-[65px] lg:py-[50px] ">
                         <div className="logo_socials flex flex-col items-center">
@@ -39,7 +39,7 @@ function Footer() {
                         <Subscribe />
                     </div>
                 </div>
-                <div className="bottom flex  flex-row flex-wrap gap-[5px] items-start justify-between relative lg:border-b-2 lg:border-opacity-20 lg:border-primary lg:gap-[63px] lg:items-center ">
+                <div className="bottom flex  flex-row flex-wrap gap-[5px] items-start justify-between relative lg:border-b-2 lg:border-opacity-20 lg:border-primary lg:gap-[20px] 2xl:gap-[63px] lg:items-center ">
 
                     <div className="company flex flex-col gap-[5px] my-8">
                         <h3 className='font-bold mb-2'>Company</h3>
@@ -72,7 +72,7 @@ function Footer() {
 
                     </div>
                     <div className="cont  lg:border-l-2 lg:border-opacity-20 lg:border-primary lg:pl-20  ">
-                        <div className="contact bg-secondary flex flex-col my-4 justify-center w-[400px] rounded-md lg:my-8  px-[45px] py-[25px]  ">
+                        <div className="contact bg-secondary flex flex-col my-4 justify-center w-[400px] rounded-md lg:my-8 px-[20px] py-[25px] lg:px-[45px] lg:py-[25px]  ">
                             <h3 className='font-bold mb-2'>Contact Us</h3>
                             <div className="wrapper flex flex-col gap-2">
                                 <div className="address flex items-start gap-2">
--- src/Components/Subscribe.js
+++ src/Components/Subscribe.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Subscribe() {
     return (
-        <div className='flex flex-wrap items-center gap-3 '>
-            <input type="email" name="email" id="email" placeholder='your email address here' className='w-[338px] text-[13px] py-[13px] border-2 pl-4 rounded-sm focus:outline-none focus:shadow-md' />
+        <div className='flex flex-wrap items-center gap-3  '>
+            <input type="email" name="email" id="email" placeholder='your email address here' className=' w-screen text-[13px] py-[13px] border-2 pl-4 rounded-sm focus:outline-none focus:shadow-md lg:w-[338px] ' />
             <button type='submit' className='w-screen bg-primary text-white text-[13px] font-medium py-[13px] px-[18px] rounded-sm hover:bg-[#26253A] transition-colors ease-in-out duration-300 md:w-fit'>Subscribe Now</button>
         </div>
     )
